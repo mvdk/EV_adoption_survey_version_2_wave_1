ab88c2b0148e956ca7d574141f14b017bc3b537d
mvdk <martvdkam@gmail.com>
2025-11-04 16:26:57 +0100

    Add files via upload

---
+++ End/lexicon_de.py
@@ -0,0 +1,6 @@
+class Lexicon:
+    end_title = 'Ende'
+    thanks = "Vielen Dank für Ihre Teilnahme."
+    wave2 = "Dies ist das Ende der ersten Phase der Studie. In wenigen Tagen werden Sie zur Teilnahme an einer zweiten Phase eingeladen. Unabhängig davon, ob Sie an der zweiten Phase teilnehmen, erhalten Sie eine Vergütung für Ihre Teilnahme an dieser ersten Phase."
+    redirect_instructions = "BITTE KLICKEN SIE UNTEN, UM DIE BEFRAGUNG  ABZUSCHLIESSEN UND IHRE ERGEBNISSE EINZUREICHEN"
+    finish = "Hier klicken, um zu beenden"
---
+++ End/lexicon_en.py
@@ -0,0 +1,6 @@
+class Lexicon:
+    end_title = 'End'
+    thanks = "Thank you for your participation."
+    wave2 = "This is the end of the first wave of the study. You will be invited to participate in a second wave in a few days. Regardless of whether you participate in the second wave, you will receive a payment for your participation in this first wave."
+    redirect_instructions = "PLEASE CLICK BELOW TO FINISH AND SUBMIT YOUR RESULTS"
+    finish = "Click here to Finish"
